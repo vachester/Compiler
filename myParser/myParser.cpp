3614804ae51fbd53a3a4b968213ed5e029f74c92
vachester <xcchester@gmail.com>
2017-04-03 04:19:40 +0800

    get the status

--- myParser/myParser.cpp
+++ myParser/myParser.cpp
@@ -1,24 +1,45 @@
 #include <iostream>
 #include "myParser.h"
 
+
+bool Project::operator==(Project &right) {
+	if(this->pro_num == right.pro_num && this->dot_position == right.dot_position && this->successors.size() == right.successors.size()) {
+		for(auto t : right.successors) {
+			if(this->successors.find(t) == this->successors.end())
+				return false;
+		}
+
+		return true;
+	}
+
+	return false;
+}
+
 Parser::Parser(){
 	//初始化非终结符和终结符
-	V = {"E", "T", "F", "M", "N"};
-	T = {"+" ,"*", "(", ")", "id", "ε"};
+	V = {"K", "S", "L", "R"};
+	T = {"=" ,"*", "i"};
 
 	std::unordered_set<std::string> empty_set;
+	std::unordered_set<int> empty_set_int;
+
 	for(auto i : V) { 
 		first.insert({i, empty_set});
 		follow.insert({i, empty_set});
+		index.insert({i,empty_set_int});
 	}
 
-	follow["E"].insert("#");
+	follow["S"].insert("#");
 }
 
 
 void Parser::get_grammer(std::ifstream &in) {
 	std::string tmp;
 	Grammer gtmp;
+	gtmp.left = "K";
+	gtmp.right = {"S"};
+	G.push_back(gtmp);
+
 	while(in >> tmp) {
 		gtmp.left = tmp;
 		gtmp.right.clear();
@@ -29,6 +50,19 @@ void Parser::get_grammer(std::ifstream &in) {
 		}
 		G.push_back(gtmp);
 	}
+	
+	for(int i = 0; i < static_cast<int>(G.size()); i++) 
+		index[G[i].left].insert(i);	
+
+	/*
+	for(auto gg : index) {
+		std::cout << gg.first << ":";
+		for(auto hh : gg.second)
+			std::cout << hh << " ";
+		std::cout << std::endl;
+	}
+	*/
+
 }
 
 bool Parser::inVT(std::string s) {
@@ -59,19 +93,19 @@ void Parser::get_first() {
 						first[g.left].insert(g.right[t]);
 						change = true;
 					}
-				} else {
-					for(auto i : first[g.right[t]]) {
-						if(first[g.left].find(i) == first[g.left].end()) {
-							first[g.left].insert(i);
-							change = true;
-						}
+					continue;
+				}  
+				for(auto i : first[g.right[t]]) {
+					if(first[g.left].find(i) == first[g.left].end()) {
+						first[g.left].insert(i);
+						change = true;
 					}
+				}
 
-					if(first[g.right[t]].find("ε") != first[g.right[t]].end()) {
-						is_empty = true;
-						t++;
-					}
-				}		
+				if(first[g.right[t]].find("ε") != first[g.right[t]].end()) {
+					is_empty = true;
+					t++;
+				}
 			}
 			if(t == static_cast<int>(g.right.size()) && first[g.left].find("ε") == first[g.left].end()) {
 				first[g.left].insert("ε");
@@ -79,6 +113,16 @@ void Parser::get_first() {
 			}
 		}
 	}
+
+	first.erase("K");
+	/*
+	for(auto gg : first) {
+		std::cout << gg.first << ":";
+		for(auto hh : gg.second) 
+			std::cout << hh << " ";
+		std::cout << std::endl;
+	}
+	*/
 }
 
 void Parser::judge_first(std::vector<std::string> s, std::unordered_set<std::string> &result) {
@@ -91,11 +135,12 @@ void Parser::judge_first(std::vector<std::string> s, std::unordered_set<std::str
 		if(first[i].find("ε") == first[i].end()) {
 			result.insert(first[i].begin(),first[i].end());
 			break;
-		} else {
-			result.insert(first[i].begin(), first[i].end());
-			result.erase("ε");
-			count++;
-		}
+		} 
+
+		result.insert(first[i].begin(), first[i].end());
+		result.erase("ε");
+		count++;
+	
 	}
 
 	if(count == static_cast<int>(s.size()))
@@ -111,30 +156,206 @@ void Parser::get_follow() {
 		std::vector<std::string>::iterator search_next; //向后扫描指针
 		for(auto &g : G) {
 			for(auto it = g.right.begin(); it != g.right.end(); it++) {
-				if(inVT(*it)) {
-					auto original = follow[*it].size();
-					search_next = it + 1;
-					if(search_next != g.right.end()) {
-						std::vector<std::string> tmp(search_next,g.right.end());	
-						std::unordered_set<std::string> stmp;
-						judge_first(tmp,stmp);
-						follow[*it].insert(stmp.begin(),stmp.end());
-						if(stmp.find("ε") != stmp.end()) {
-							follow[*it].erase("ε");
-							follow[*it].insert(follow[g.left].begin(),follow[g.left].end());
-						} 
-						if(original < follow[*it].size())
-							change = true;
-					} else {
-						follow[*it].insert(follow[g.left].begin(), follow[g.left].end());
-						if(original < follow[*it].size())
-							change = true;
-					}
+				if(!inVT(*it)) 
+					continue;
+				auto original = follow[*it].size();
+				search_next = it + 1;
+				if(search_next != g.right.end()) {
+					std::vector<std::string> tmp(search_next,g.right.end());	
+					std::unordered_set<std::string> stmp;
+					judge_first(tmp,stmp);
+					follow[*it].insert(stmp.begin(),stmp.end());
+					if(stmp.find("ε") != stmp.end()) {
+						follow[*it].erase("ε");
+						follow[*it].insert(follow[g.left].begin(),follow[g.left].end());
+					} 
+					if(original < follow[*it].size())
+						change = true;
+				} else {
+					follow[*it].insert(follow[g.left].begin(), follow[g.left].end());
+					if(original < follow[*it].size())
+						change = true;
 				}
 			}
 		}
 	}
+
+	follow.erase("K");
+	/*
+	for(auto gg : follow) {
+		std::cout << gg.first << ":";
+		for(auto hh : gg.second) 
+			std::cout << hh << " ";
+		std::cout << std::endl;
+	}
+	*/
 }
+
+void Parser::get_closure(Status &p) {
+	bool change = true;
+	Status pptmp;
+	std::unordered_set<int> record; //记录已经求过的产生式
+	while(change) {
+		change = false;
+		pptmp = p;
+		for(auto &pro : pptmp) {
+			if(!(record.find(pro.pro_num) == record.end() && pro.dot_position < static_cast<int>(G[pro.pro_num].right.size()))) 
+				continue;
+			record.insert(pro.pro_num);
+			std::string V = G[pro.pro_num].right[pro.dot_position];
+			if(!inVT(V)) 
+				continue;
+			std::unordered_set<std::string> new_successor;
+
+			//求新项目的后继符集
+			if(pro.dot_position == static_cast<int>(G[pro.pro_num].right.size()) - 1)
+				new_successor = pro.successors;
+			else {
+				std::vector<std::string> vtmp(G[pro.pro_num].right.begin()+pro.dot_position+1,G[pro.pro_num].right.end()); 
+				judge_first(vtmp, new_successor);
+				if(new_successor.find("ε") != new_successor.end()) {
+					new_successor.insert(pro.successors.begin(),pro.successors.end());
+					new_successor.erase("ε");
+				}
+			}
+			Project ptmp;
+			for(auto &i : index[V]) {
+				ptmp.pro_num = i;
+				ptmp.dot_position = 0;
+				ptmp.successors = new_successor;
+				if(p.find(ptmp) == p.end()) {
+					p.insert(ptmp);
+					change = true;
+				}
+				else {
+					auto original = new_successor.size(); 
+					ptmp.successors.insert(p.find(ptmp)->successors.begin(),p.find(ptmp)->successors.end());
+					p.erase(*p.find(ptmp));
+					p.insert(ptmp);
+
+					if(original < ptmp.successors.size())
+						change = true;
+				}	
+			}
+		}
+	}
+/*
+   for(auto jj : p) {
+   std::cout << jj.pro_num << " " << jj.dot_position << ",";
+   for(auto kk : jj.successors)
+   std::cout << kk << " ";
+   std::cout << std::endl;
+   }
+   */
+}
+
+bool Parser::judge_repeat(Status s1, Status s2) {
+	if(s1.size() == s2.size()) {
+		for(auto pros : s1) {
+			if(s2.find(pros) == s2.end())
+				return false;	
+			else {
+				Project tmp = *(s2.find(pros));
+				if(!(tmp == pros))
+					return false;
+			}
+		}
+		return true;
+	}
+	return false;
+}
+
+
+
+void Parser::get_status() {
+	//初始化第一个状态0
+	int t = 0;
+	Project ptmp;
+	ptmp.dot_position = 0;
+	ptmp.pro_num = 0;
+	ptmp.successors = {"#"};
+	Status tmp_status(10,hasher,eq);
+	tmp_status.insert(ptmp);
+	get_closure(tmp_status);
+
+	statuses.insert({t,tmp_status});
+
+	//获取状态集合
+	bool change = true;
+	std::unordered_set<int> record; //记录已经求过的状态
+	std::unordered_map<int, Status> sstmp;
+
+	while(change) {
+		change = false;
+		sstmp = statuses;
+
+		for(auto &sta : sstmp) {
+			if(record.find(sta.first) != record.end()) 
+				continue;
+			record.insert(sta.first);
+
+			/*
+			 * 开始球状态转移，用一个set<string>记录被转移过的状态
+			 */
+			std::unordered_set<std::string> record_status;
+			for(auto pros : sta.second) {
+				if(pros.dot_position == static_cast<int>(G[pros.pro_num].right.size())) {
+					for(auto sucess : pros.successors)
+						action[sta.first][sucess] = "r" + std::to_string(pros.pro_num);
+					continue;
+				}
+				std::string trans = G[pros.pro_num].right[pros.dot_position];
+				if(record_status.find(trans) != record_status.end()) 
+					continue;
+				record_status.insert(trans);
+				tmp_status.clear();
+				ptmp.pro_num = pros.pro_num;
+				ptmp.dot_position = pros.dot_position + 1;
+				ptmp.successors = pros.successors;
+				tmp_status.insert(ptmp);
+				for(auto protmp : sta.second) {
+					if(G[protmp.pro_num].right[protmp.dot_position] == trans && !(protmp == pros)) {
+						ptmp.pro_num = protmp.pro_num;
+						ptmp.dot_position = protmp.dot_position + 1;
+						ptmp.successors = protmp.successors;
+						tmp_status.insert(ptmp);
+					}
+				}
+				get_closure(tmp_status);
+				bool flag = true;
+				for(auto s : sstmp) {
+					if(judge_repeat(s.second,tmp_status)) {
+						if(inVT(trans))
+							goTo[sta.first][trans] = s.first;
+						else
+							action[sta.first][trans] = "s" + std::to_string(s.first);
+						flag = false;
+						break;
+					}							
+				}
+				if(!flag) 
+					continue;
+				statuses.insert({++t, tmp_status});
+				change = true;
+
+				//填写goTo表和action表
+				if(inVT(trans)) 
+					goTo[sta.first][trans] = t;
+				else 
+					action[sta.first][trans] = "s" + std::to_string(t);
+			}
+		}
+	}
+	/*
+	   for(auto ttt : goTo) {
+	   std::cout << ttt.first << "{ " ;
+	   for(auto kkk : ttt.second) 
+	   std::cout << kkk.first << ":" << kkk.second << ",";
+	   std::cout << " }" << std::endl;
+	   }
+	   */
+}
+
 
 int main() {
 	Parser test;
@@ -142,6 +363,6 @@ int main() {
 	test.get_grammer(in);
 	test.get_first();
 	test.get_follow();
-
+	test.get_status();
 	return 0;
 }
